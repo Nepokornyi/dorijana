d2390634b84f5a0380143f07fbce80e6a58a2f33
Nepokornyi <vadymkin@gmail.com>
2025-05-31 23:51:59 +0200

    add landing video

---
+++ app/components/Landing/Landing.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Header } from '../Header'
+import { H1, H3 } from '@/components/ui/typography'
+import { Button } from '@/components/ui/button'
+import { FlexContainer } from '@/components/ui/flexContainer'
+import { Box } from '@/components/ui/box'
+
+export const Landing = () => {
+    return (
+        <Box>
+            <video
+                src="/video/landing.mp4"
+                muted
+                loop
+                autoPlay
+                className="absolute top-0 left-0 -z-1 w-full h-full object-cover"
+            />
+            <FlexContainer
+                justifyContent="justify-center"
+                direction="flex-col"
+                gap="gap-4"
+                className="pl-60 h-screen"
+            >
+                <Header />
+                <Box>
+                    <Button>Check out our feature news</Button>
+                </Box>
+                <H1>A new way of building</H1>
+                <H3>Quick and closer to nature</H3>
+            </FlexContainer>
+        </Box>
+    )
+}
