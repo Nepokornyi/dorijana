571830d35e5c44b136e87c764978a34ab78488a9
Nepokornyi <vadymkin@gmail.com>
2025-05-31 23:51:03 +0200

    updated header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,22 +1,26 @@
+import { Box } from '@/components/ui/box'
 import { Button } from '@/components/ui/button'
+import { H2 } from '@/components/ui/typography'
 import React from 'react'
+import { ThemeChange } from './Landing/ThemeChange'
 
 export const Header = () => {
     return (
-        <header className="flex justify-between items-center">
-            <div>logo</div>
-            <div>
+        <header className="absolute top-0 left-0 w-full flex justify-between items-center p-10">
+            <H2 className="uppercase tracking-widest border-none">dorijana</H2>
+            <Box>
                 <Button variant={'link'}>Live</Button>
                 <Button variant={'link'}>Work</Button>
                 <Button variant={'link'}>Relax</Button>
                 <Button variant={'link'}>Care</Button>
-            </div>
-            <div>
+            </Box>
+            <Box>
                 <Button variant={'link'}>About</Button>
                 <Button variant={'link'}>News</Button>
                 <Button variant={'link'}>Contact</Button>
                 <Button variant={'link'}>Projects</Button>
-            </div>
+                <ThemeChange />
+            </Box>
         </header>
     )
 }
