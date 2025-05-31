5b23082d5a70be3137f45930f7553e5fb71320e6
Nepokornyi <vadymkin@gmail.com>
2025-05-31 23:51:14 +0200

    theme change component

---
+++ app/components/Landing/ThemeChange.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+
+import React, { useCallback } from 'react'
+
+export const ThemeChange = () => {
+    const toggleDarkMode = useCallback(() => {
+        const html = document.documentElement
+        html.classList.toggle('dark')
+    }, [])
+
+    return (
+        <Button variant={'default'} onClick={() => toggleDarkMode()}>
+            Theme
+        </Button>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,12 @@
-'use client'
-import { Button } from '@/components/ui/button'
-import { useCallback } from 'react'
-import { Landing } from './components/Landing'
+import { Landing } from './components/Landing/Landing'
 import { About } from './components/About'
 import { Work } from './components/Work'
 import { Projects } from './components/Projects'
 import { Contact } from './components/Contact'
 
 export default function Home() {
-    const toggleDarkMode = useCallback(() => {
-        const html = document.documentElement
-        html.classList.toggle('dark')
-    }, [])
-
     return (
-        <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
-            <Button variant={'secondary'} onClick={() => toggleDarkMode()}>
-                Theme
-            </Button>
+        <div className="font-[family-name:var(--font-geist-sans)]">
             <Landing />
             <About />
             <Work />
