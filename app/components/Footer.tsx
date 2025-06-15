79d708f420ab68f83b6aa67d4794bb735c846ede
Nepokornyi <vadymkin@gmail.com>
2025-06-15 20:16:30 +0200

    add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,23 @@
+import { Box } from '@/components/ui/box'
+import { H3 } from '@/components/ui/typography'
+import React from 'react'
+
+const gridData = [
+    { text: 'Company', className: 'px-20 py-10' },
+    { text: 'Contact', className: 'py-10' },
+]
+
+export const Footer = () => {
+    return (
+        <footer className="flex flex-col items-center">
+            <Box className="w-5/6 pl-[15%] grid grid-cols-3 gap-10">
+                {gridData.map((col) => (
+                    <Box key={col.text} className={col.className}>
+                        <H3>{col.text}</H3>
+                    </Box>
+                ))}
+            </Box>
+            Dorijana s.r.o. © 2002-2025
+        </footer>
+    )
+}
