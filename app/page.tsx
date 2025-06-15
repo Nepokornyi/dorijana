0dd0bc554ea0cb68d82b98887bb321fc80c30fa0
Nepokornyi <vadymkin@gmail.com>
2025-06-15 20:16:40 +0200

    add mising layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,20 @@ import { Landing } from './components/Landing/Landing'
 import { Intro } from './components/Intro'
 import { Work } from './components/Work'
 import { Projects } from './components/Projects'
-import { Contact } from './components/Contact'
+import { Footer } from './components/Footer'
 import { About } from './components/About'
+import { Carousel } from './components/Carousel'
 
 export default function Home() {
     return (
-        <div className="font-[family-name:var(--font-geist-sans)]">
+        <div className="font-[family-name:var(--font-geist-mono)]">
             <Landing />
             <Intro />
             <About />
             <Work />
+            <Carousel />
             <Projects />
-            <Contact />
+            <Footer />
         </div>
     )
 }
