7c3a3c53ab173a20228c989f9c5f7bac937058b5
Nepokornyi <vadymkin@gmail.com>
2025-12-07 20:29:12 +0100

    basic footer layout

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,23 +1,56 @@
 import { Box } from '@/components/ui/box'
-import { H3 } from '@/components/ui/typography'
-import React from 'react'
+import { FlexContainer } from '@/components/ui/flexContainer'
+import { TypographyMuted } from '@/components/ui/typography'
 
 const gridData = [
-    { text: 'Company', className: 'px-20 py-10' },
-    { text: 'Contact', className: 'py-10' },
+    {
+        header: 'Dorijana',
+        text: 'Dorijana s.r.o. je pražská stavební společnost, která provádí stavební zakázky ve vysoké kvalitě s velkým důrazem na detaily.',
+        className: '',
+    },
+    {
+        header: 'Kontaktujte nás',
+        points: [
+            'info@dorijana.cz',
+            '+420 777 700 202',
+            'K Červenému dvoru 175/18a',
+            'Strašnice, 100 00 Praha 10',
+        ],
+        className: '',
+    },
+    {
+        header: 'Důležité odkazy',
+        text: 'Podmínky ochrany osobních údajů',
+        className: '',
+    },
 ]
 
 export const Footer = () => {
     return (
-        <footer className="flex flex-col items-center">
-            <Box className="w-full lg:w-5/6 lg:pl-[15%] grid grid-cols-2 lg:grid-cols-3 gap-10">
+        <footer className="w-full flex flex-col items-center py-10">
+            <Box className="w-full lg:pl-60 grid grid-cols-2 lg:grid-cols-3 gap-10">
                 {gridData.map((col) => (
-                    <Box key={col.text} className={col.className}>
-                        <H3>{col.text}</H3>
-                    </Box>
+                    <FlexContainer
+                        direction="flex-col"
+                        gap="gap-4"
+                        key={col.text}
+                        className={`${col.className} 'px-10 lg:px-0 py-10`}
+                    >
+                        <TypographyMuted>{col.header}</TypographyMuted>
+                        {col.text && (
+                            <TypographyMuted>{col.text}</TypographyMuted>
+                        )}
+                        <ul>
+                            {col.points?.map((point) => (
+                                <li>
+                                    <TypographyMuted>{point}</TypographyMuted>
+                                </li>
+                            ))}
+                        </ul>
+                    </FlexContainer>
                 ))}
             </Box>
-            Dorijana s.r.o. © 2002-2025
+            <TypographyMuted> Dorijana s.r.o. © 2002-2025</TypographyMuted>
         </footer>
     )
 }
