afb6d502a05136d52fe1b99e6ec72ed630242ca4
vadym.kostucok <vadym.kostucok@gmail.com>
2026-01-16 22:33:49 +0100

    update animation opacity

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -56,7 +56,7 @@ const textLeftVariants: Variants = {
 const textRightVariants: Variants = {
     hidden: { opacity: 0, x: 40 },
     visible: {
-        opacity: 1,
+        opacity: 0.75,
         x: 0,
         transition: {
             duration: 1.2,
