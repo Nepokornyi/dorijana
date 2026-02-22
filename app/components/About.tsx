3ccaf123853fce42e611d80ab38242f5555537c7
Nepokornyi <vadymkin@gmail.com>
2026-02-23 00:08:06 +0100

    fix gap

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -87,7 +87,7 @@ export const About = () => {
                     </Box>
                 ))}
             </MotionBox>
-            <MotionBox className="w-full lg:px-32 xl:px-60 pb-10 grid grid-cols-1 lg:grid-cols-3 gap-x-10 gap-y-2.5">
+            <MotionBox className="w-full lg:px-32 xl:px-60 pb-10 grid grid-cols-1 lg:grid-cols-3 gap-10">
                 <MotionH3
                     variants={textLeftVariants}
                     className="hidden lg:block px-10 lg:px-0 py-10"
@@ -96,7 +96,7 @@ export const About = () => {
                 </MotionH3>
                 <MotionH3
                     variants={textRightVariants}
-                    className="px-10 lg:px-0 py-10 lg:col-span-2 lg:text-justify"
+                    className="px-10 lg:px-0 pt-10 lg:col-span-2 lg:text-justify"
                 >
                     {t('paragraph1')}
                 </MotionH3>
