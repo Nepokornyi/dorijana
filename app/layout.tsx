10e58aed4df85e0ae7660d249775532bf2682c2e
Nepokornyi <vadymkin@gmail.com>
2026-02-22 21:24:39 +0100

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import { headers } from 'next/headers'
+import Script from 'next/script'
 import { Gantari, Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { routing } from '@/i18n/routing'
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -32,6 +35,25 @@ export default async function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} ${gantariSans.variable} antialiased`}
             >
+                {GA_ID && (
+                    <>
+                        <Script
+                            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+                            strategy="afterInteractive"
+                        />
+                        <Script
+                            id="google-analytics"
+                            strategy="afterInteractive"
+                        >
+                            {`
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', '${GA_ID}');
+                            `}
+                        </Script>
+                    </>
+                )}
                 {children}
             </body>
         </html>
